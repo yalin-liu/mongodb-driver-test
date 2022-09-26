f93e7fcfa887bf74ad8446e7eb52faf159a119e7
yalin-liu <42903384+yalin-liu@users.noreply.github.com>
2022-09-26 13:15:48 +0800

    Update server.js

--- server.js
+++ server.js
@@ -6,14 +6,14 @@ const dbName = 'test'; // Database Name
 
 const countRestaurants = function(db, callback) {
 	var collection = db.collection('restaurants');
-	collection.countDocuments(function(err,count) {
+	collection.countDocuments(function(err,count) { //`countdocuments` operation in MongoDB
 		assert.equal(null,err);
 		console.log(`There are ${count} documents in the restuarant collection`);
 	})
 	callback();
 }
 
-const client = new MongoClient(url);
+const client = new MongoClient(url); // invoking the const MongoCLient, the first executable statement
 
 client.connect(function(err) {
    assert.equal(null,err);
